1be61cde2f040bb643ff5fb597052673c299ef8a
skye-pa1n <sonicgames772@gmail.com>
2025-12-10 10:15:18 -0300

    Revert "Reapply "sched: Resolve sched_feat() at compile time to improve code optimization""
    This reverts commit 63600506699d8f825db06881cc8021877a09c09f.

--- kernel/smp.c
+++ kernel/smp.c
@@ -22,7 +22,12 @@
 #include <linux/suspend.h>
 
 #include "smpboot.h"
-#include "sched/features.h"
+
+enum {
+	CSD_FLAG_LOCK		= 0x01,
+	CSD_FLAG_SYNCHRONOUS	= 0x02,
+};
+
 #include "sched/smp.h"
 
 #define CSD_TYPE(_csd)	((_csd)->flags & CSD_FLAG_TYPE_MASK)
