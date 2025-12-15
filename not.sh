15cb557cba44595e8a36d0e030b47b0cfdbf3f41
skye-pa1n <sonicgames772@gmail.com>
2025-12-15 14:23:30 -0300

    not: Aviva

--- not.sh
+++ not.sh
@@ -2,7 +2,7 @@
 LLVM_PATH="/home/skye/toolchains/neutron-clang/bin/"
 LLVM_PATH="/home/skye/toolchains/neutron-clang/bin/"
 
-KERNEL_NAME="chaoticn't"
+KERNEL_NAME="Aviva"
 
 HOST_BUILD_ENV="ARCH=arm64 \
                 CC=${LLVM_PATH}clang \
