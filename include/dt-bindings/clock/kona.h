dcd4587eddce48abbd801c6f10859e20b4aa5453
skye-tachyon <sonicgames772@gmail.com>
2026-03-31 23:06:38 -0300

    include: dt-bindings: add kona.h
    * tmp
    
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

---
+++ include/dt-bindings/clock/kona.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <dt-bindings/clock/qcom,gcc-kona.h>
+#include <dt-bindings/clock/qcom,camcc-kona.h>
+#include <dt-bindings/clock/qcom,cpucc-kona.h>
+#include <dt-bindings/clock/qcom,gpucc-kona.h>
+#include <dt-bindings/clock/qcom,npucc-kona.h>
+#include <dt-bindings/clock/qcom,dispcc-kona.h>
+#include <dt-bindings/clock/qcom,videocc-kona.h>
