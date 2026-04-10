fc051ec2293c050cfeb59295df2301fab942be84
skye-tachyon <sonicgames772@gmail.com>
2026-04-10 20:44:32 -0300

    drivers: devfreq: Remove SIMPLE_GPU_ALGORITHM changes
    * kek
    
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- drivers/gpu/msm/governor_msm_adreno_tz.c
+++ drivers/gpu/msm/governor_msm_adreno_tz.c
@@ -56,7 +56,7 @@ static DEFINE_SPINLOCK(tz_lock);
 #define TAG "msm_adreno_tz: "
 
 #if 1
-static unsigned int adrenoboost = 0;
+static unsigned int adrenoboost = 1;
 #endif
 
 static atomic_long_t suspend_time;
@@ -401,12 +401,6 @@ static inline int devfreq_get_freq_level(struct devfreq *devfreq,
 	return -EINVAL;
 }
 
-#ifdef CONFIG_SIMPLE_GPU_ALGORITHM
-extern int simple_gpu_active;
-extern int simple_gpu_algorithm(int level, int *val,
-				struct devfreq_msm_adreno_tz_data *priv);
-#endif
-
 #if 1
 /* Adreno idler stuff */
 // mapping gpu level calculated linear conservation half curve values into a
@@ -508,18 +502,6 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 			priv->bin.busy_time > CEILING) {
 		val = -1 * level;
 	} else {
-#ifdef CONFIG_SIMPLE_GPU_ALGORITHM
-		if (simple_gpu_active) {
-			simple_gpu_algorithm(level, &val, priv);
-		} else {
-			scm_data[0] = level;
-			scm_data[1] = priv->bin.total_time;
-			scm_data[2] = priv->bin.busy_time;
-			scm_data[3] = context_count;
-			__secure_tz_update_entry3(scm_data, sizeof(scm_data),
-						&val, sizeof(val), priv);
-		}
-#else
 		unsigned int refresh_rate = dsi_panel_get_refresh_rate();
 		
 		scm_data[0] = level;
@@ -532,7 +514,6 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq)
 		scm_data[3] = context_count;
 		__secure_tz_update_entry3(scm_data, sizeof(scm_data),
 					&val, sizeof(val), priv);
-#endif
 	}
 #if 0
 	priv->bin.total_time = 0;
