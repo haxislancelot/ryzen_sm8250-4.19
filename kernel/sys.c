dccc9fce2830a4aaa83fe13936f41eaae71eae96
skye-tachyon <sonicgames772@gmail.com>
2026-04-13 20:11:41 -0300

    kernel: syscall: Spoof for uprobestats as well
    Change-Id: I98110036217e001a0ffa1f6de212bb02c6b3a6d2
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1254,7 +1254,8 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
             !strncmp(current->comm, "uprobestatsbpfload", 18) ||
-	    !strncmp(current->comm, "netd", 4)) {
+	    !strncmp(current->comm, "netd", 4) ||
+	    !strncmp(current->comm, "uprobestats", 11)) {
 	       if (current_uid().val == 0) {
 		       strcpy(tmp.release, "5.10.239");
 		       pr_debug("fake uname: %s/%d release=%s\n",
