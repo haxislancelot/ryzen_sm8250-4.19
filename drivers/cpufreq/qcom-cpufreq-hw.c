1582130940/android_kernel_xiaomi_sdm660

cpufreq/qcom-cpufreq-hw: Force qcom-cpufreq-hw to init early
In non-GKI configs, the qcom-cpufreq-hw driver initializes before first
stage init begins, bringing the system quickly to a higher frequency. In
GKI configurations, however, this does not happen until fairly late in
first stage init, which contributes to boot time regressions. Force this
module to load earlier and provide some default clock rates to alleviate
the driver's dependency on various clocks, which it uses only once
during init to get their rates.

Bug: 157932380
Signed-off-by: J. Avila <elavila@google.com>
Change-Id: I6b6e6826b1caaa9d5fa59943e816494622e12c87

drivers/cpufreq/qcom-cpufreq-hw.c
@@ -644,6 +644,8 @@ static int qcom_cpu_resources_init(struct platform_device *pdev,
 	return 0;
 }
 
+#define DEFAULT_XO_RATE 19200000
+#define DEFAULT_ALT_RATE 600000000
 static int qcom_resources_init(struct platform_device *pdev)
 {
 	struct device_node *cpu_np;
@@ -654,20 +656,26 @@ static int qcom_resources_init(struct platform_device *pdev)
 	int ret;
 
 	clk = devm_clk_get(&pdev->dev, "xo");
-	if (IS_ERR(clk))
-		return PTR_ERR(clk);
-
-	xo_rate = clk_get_rate(clk);
-
-	devm_clk_put(&pdev->dev, clk);
+	if (IS_ERR(clk)) {
+		xo_rate = DEFAULT_XO_RATE;
+		dev_dbg(&pdev->dev, "XO clock get failed: %d, using default " \
+			"rate %lu\n", PTR_ERR(clk), xo_rate);
+	} else {
+		xo_rate = clk_get_rate(clk);
+		devm_clk_put(&pdev->dev, clk);
+	}
 
 	clk = devm_clk_get(&pdev->dev, "alternate");
-	if (IS_ERR(clk))
-		return PTR_ERR(clk);
+	if (IS_ERR(clk)) {
+		cpu_hw_rate = DEFAULT_ALT_RATE;
+		dev_dbg(&pdev->dev, "Alternate clock get failed: %d, using " \
+			"default rate %lu\n", PTR_ERR(clk), cpu_hw_rate);
+	} else {
+		cpu_hw_rate = clk_get_rate(clk);
+		devm_clk_put(&pdev->dev, clk);
+	}
 
-	cpu_hw_rate = clk_get_rate(clk) / CLK_HW_DIV;
-
-	devm_clk_put(&pdev->dev, clk);
+	cpu_hw_rate /= CLK_HW_DIV;
 
 	of_property_read_u32(pdev->dev.of_node, "qcom,lut-row-size",
 			      &lut_row_size);

haxislancelot/ryzen_sm8250-4.19

Signed-off-by: ris <rissu.ntk@gmail.com>
Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

drivers/cpufreq/qcom-cpufreq-hw.c
@@ -744,6 +744,8 @@ static int qcom_cpu_resources_init(struct platform_device *pdev,
 	return 0;
 }
 
+#define DEFAULT_XO_RATE 19200000
+#define DEFAULT_ALT_RATE 600000000
 static int qcom_resources_init(struct platform_device *pdev)
 {
 	struct device_node *cpu_np;
@@ -754,20 +756,26 @@ static int qcom_resources_init(struct platform_device *pdev)
 	int ret;
 
 	clk = devm_clk_get(&pdev->dev, "xo");
-	if (IS_ERR(clk))
-		return PTR_ERR(clk);
-
-	xo_rate = clk_get_rate(clk);
-
-	devm_clk_put(&pdev->dev, clk);
+	if (IS_ERR(clk)) {
+		xo_rate = DEFAULT_XO_RATE;
+		dev_dbg(&pdev->dev, "XO clock get failed: %ld, using default " \
+			"rate %lu\n", PTR_ERR(clk), xo_rate);
+	} else {
+		xo_rate = clk_get_rate(clk);
+		devm_clk_put(&pdev->dev, clk);
+	}
 
 	clk = devm_clk_get(&pdev->dev, "alternate");
-	if (IS_ERR(clk))
-		return PTR_ERR(clk);
+	if (IS_ERR(clk)) {
+		cpu_hw_rate = DEFAULT_ALT_RATE;
+		dev_dbg(&pdev->dev, "Alternate clock get failed: %ld, using " \
+			"default rate %lu\n", PTR_ERR(clk), cpu_hw_rate);
+	} else {
+		cpu_hw_rate = clk_get_rate(clk);
+		devm_clk_put(&pdev->dev, clk);
+	}
 
-	cpu_hw_rate = clk_get_rate(clk) / CLK_HW_DIV;
-
-	devm_clk_put(&pdev->dev, clk);
+	cpu_hw_rate /= CLK_HW_DIV;
 
 	of_property_read_u32(pdev->dev.of_node, "qcom,lut-row-size",
 			      &lut_row_size);