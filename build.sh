3ead07547e205d9a4aba6eedb3b09424a86a40b3
skye-tachyon <sonicgames772@gmail.com>
2026-04-19 14:52:45 -0300

    build: use mkdir -p
    Change-Id: Ide8b6d3fcb5f484b5874a374be96b152855512d2
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ export PATH="$TC_DIR/bin:$PATH"
 
 if ! [ -d "$TC_DIR" ]; then
     echo -e "${YELLOW} Clang not found! Cloning to $TC_DIR...${NC}"
-    mkdir $TC_DIR
+    mkdir -p "$TC_DIR"
     if ! curl -L https://github.com/skye-tachyon/clangbuiltlinux/releases/download/22-1-3-rel/clang-22-1-3.tar.gz \
         | tar -xz -C "$TC_DIR" --strip-components=1; then
         echo -e "${RED}Cloning failed! Aborting...${NC}"
